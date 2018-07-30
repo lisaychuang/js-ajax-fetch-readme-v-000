50bd933f25be4811ce4b88939f88cef20979fc41
Lisa Huang <lisa.yc.huang@gmail.com>
2018-07-29 20:20:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much.";
+const token = " ";
+
+function getRepos(){
+    fetch("https://api.github.com/users/:user/repos", {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
+}
